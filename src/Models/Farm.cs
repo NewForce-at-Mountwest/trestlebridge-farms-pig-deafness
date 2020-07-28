9ae4502c0d1d698bbecff348e14c1cf32f594d68
Pat Shaver <pat.shaver@icloud.com>
2020-07-28 09:58:55 -0400

    finished naturalfield.cs

--- src/Models/Farm.cs
+++ src/Models/Farm.cs
@@ -9,6 +9,13 @@ namespace Trestlebridge.Models
     public class Farm
     {
         public List<GrazingField> GrazingFields { get; } = new List<GrazingField>();
+        //public List<PlowedField> PlowedFields { get; } = new List<PlowedField>();
+
+        public List<NaturalField> NaturalFields { get; } = new List<NaturalField>();
+
+        //public List<ChickenHouse> ChickenHouses { get; } = new List<ChickenHouse>();
+
+        //public List<DuckHouse> DuckHouses { get; } = new List<DuckHouse>();
 
         /*
             This method must specify the correct product interface of the
@@ -31,6 +38,12 @@ namespace Trestlebridge.Models
         {
             GrazingFields.Add(field);
         }
+
+        public void AddNaturalField (NaturalField field)
+        {
+            NaturalFields.Add(field);
+        }
+
 
         public override string ToString()
         {
