67f8ad827031c0978fabbc175a914aec12970d5e
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-07-28 10:24:28 -0400

    So I can check a pull request

---
+++ src/Models/Facilities/PlowedField.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Facilities
+{
+    public class PlowedField :IFacility<ISeedProducing>
+    {
+        public double Capacity { get; }
+        public void AddResource(ISeedProducing resource)
+        {
+            /*Adds seed?*/
+        }
+
+        public void AddResource(List<ISeedProducing> resources)
+        {
+            /*List of resources*/
+        }
+    }
+}
