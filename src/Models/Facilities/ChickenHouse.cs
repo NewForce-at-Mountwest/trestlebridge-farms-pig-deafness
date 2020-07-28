7b2c05874710dad230aa097a557b098e7b51e94e
Devin Conroy <d.conroy1991@gmail.com>
2020-07-28 11:40:44 -0400

    added chicken house

--- src/Models/Facilities/ChickenHouse.cs
+++ src/Models/Facilities/ChickenHouse.cs
@@ -6,6 +6,27 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Facilities {
 
-    
+public class ChickenHouse : IFacility<IResource>
+    {
+        private int _capacity = 15;
+
+        public double Capacity {
+            get {
+                return _capacity;
+            }
+        }
+
+        double IFacility<IResource>.Capacity => throw new NotImplementedException();
+
+        public void AddResource(IResource resource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddResource(List<IResource> resources)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
 }
