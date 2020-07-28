121666de8ef07e82aa12f3bbf5bf8b64023fe1d0
Pat Shaver <pat.shaver@icloud.com>
2020-07-28 11:48:14 -0400

    Need to test Devin's Pull request

--- src/Models/Plants/WildFlower.cs
+++ src/Models/Plants/WildFlower.cs
@@ -3,13 +3,13 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Plants
 {
-    public class WildFlower : IResource, ISeedProducing
+    public class WildFlower : IResource, ICompostProducing
     {
-        private int _seedsProduced = 40;
+        private double _compostProduced = 30.3;
         public string Type { get; } = "Wildflower";
 
         public double Harvest () {
-            return _seedsProduced;
+            return _compostProduced;
         }
 
         public override string ToString () {
